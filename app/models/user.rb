c513cf59e62c5911f1b2e3c107fbdc0da9e686c2
Leonardo Reina <leo.reina01@gmail.com>
2022-03-03 18:10:59 -0300

    wip photo to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_many :experiences, dependent: :destroy
   has_many :orders, dependent: :destroy
   validates :name, presence: true
+  has_one_attached :photo
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
