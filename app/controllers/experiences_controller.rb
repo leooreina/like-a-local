84b755fb93923a90c98f24906a2b48dd9d1a8a45
vitor morais <vitoramorais@hotmail.com>
2022-03-03 12:12:48 -0300

    destroy

--- app/controllers/experiences_controller.rb
+++ app/controllers/experiences_controller.rb
@@ -2,4 +2,10 @@ class ExperiencesController < ApplicationController
   def index
 
   end
+
+  def destroy
+    @experience = Experience.find(params[:id])
+    @experience.destroy
+    redirect_to experience_path(@experience)
+  end
 end
