03578ba67a64d9eed8e02651ca9e9e7a0a38def7
Leonardo Reina <leo.reina01@gmail.com>
2022-03-03 15:09:52 -0300

    creating index new create and show for orders

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :experiences do
-    resources :orders
+    resources :orders, only: %i(index new create show)
   end
   root to: 'experiences#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
