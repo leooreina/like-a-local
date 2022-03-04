47d4489881db281cadf42b9d94b68ac255e2bf9f
Leonardo Reina <leo.reina01@gmail.com>
2022-03-04 10:57:07 -0300

    photo default to user that no uploads a picture

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   has_many :experiences, dependent: :destroy
   has_many :orders, dependent: :destroy
-  validates :name, presence: true
   has_one_attached :photo
+  validates :name, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
