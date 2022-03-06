dcd29109bd2290a60cf1225daebf7801728e625f
Leonardo Reina <leo.reina01@gmail.com>
2022-03-06 20:06:52 -0300

    creating my experiences and my orders pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
+  devise_scope :user do
+    get 'my-experiences', to: 'devise/sessions#my_experiences'
+  end
+
   root to: 'experiences#index'
   resources :experiences do
     resources :orders
   end
-
- 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
 end
